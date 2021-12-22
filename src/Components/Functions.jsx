fe422c672a19f058962a3a63df2cb11d277cbbfa
BuildTools <unconfigured@null.spigotmc.org>
2021-12-22 16:27:15 -0500

    -

---
+++ src/Components/Functions.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Functions = () => {
+    return (
+        <div className="functions">
+            <button>clear</button>
+            <button>reset</button>
+        </div>
+
+    )
+}
+
+export default Functions;
